33746644fefc625fcebaf731da0e59749ce1b189
Yomaira Escano <yjescano@gmail.com>
2018-01-25 22:42:19 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < Sinatra::Base
       User.create(username: params[:username], password: params[:password])
       redirect '/login'
     end
-    # user = User.new(:username => params[:username], :password => params[:password])
+    user = User.create(:username => params[:username], :password => params[:password])
     #
     # if user.save
     #   redirect to '/login'
